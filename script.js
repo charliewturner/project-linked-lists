a31657782c7ef61a9d582aec37bfd6c82d99cae7
Charlie Turner <c.w.turner@hotmail.com>
2024-09-18 09:13:00 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const ListNode = () {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+const LinkedList = () {
+    constructor(head = null){
+        this.head = head;
+    }
+
+    const append(value) {
+
+    }
+
+    const prepend(value) {
+        
+    }
+
+    const at(index) {
+
+    }
+
+    const contains(value) {
+
+    }
+
+    const find(value) {
+
+    }
+
+    const size() {
+
+    }
+
+    const head() {
+
+    }
+
+    const tail() {
+
+    }
+
+    const pop() {
+
+    }
+
+    const toString() {
+
+    }
+}
